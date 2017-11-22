bef972a1645157c117f80ada23671afc98a683e5
Kevin ramirez <kevin.ramirez@jbge.com.mx>
2017-11-22 10:36:29 -0600

    formas de implementar acciones en elementos ui

--- app/src/main/java/com/example/android_app_test/MainActivity.java
+++ app/src/main/java/com/example/android_app_test/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.android_app_test;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.app.Activity;
 import android.widget.Toast;
@@ -15,20 +17,44 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView tv;
 
+    private Button btn;
+
+    // Se llama cuando se inicia el activity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // esta linea de abajo indica el layout xml que se usa
         setContentView(R.layout.activity_main);
         tv = (TextView) findViewById(R.id.tv_hw);
         tv.setText("Cambio de texto");
 
         // Actividades https://developer.android.com/training/basics/activity-lifecycle/starting.html
+        // Un activity es la vista de la aplicacion
         // Ver diagrama para ver como es el ciclo de vida y los onCreate, onStart, etc
         // Un activity solo puede tener un contexto
         Context context = this;
         Test.myToast(context, "onCreate");
 
+        // Metodo 1
+        // opcion mas recomendable que usar android:onClick="miMetodo" en el xml
+        btn = (Button) findViewById(R.id.buttonMain);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Button clicked!", Toast.LENGTH_LONG).show();
+            }
+        });
+
+
     }
+
+    // Metodo 2 para boton para llamar desde el xml
+    // MainActivity.this es el contexto de mainactivity
+    public void miMetodo(View v) {
+        int a = 4;
+        Toast.makeText(MainActivity.this, "valor: " + a, Toast.LENGTH_LONG).show();
+    }
+
 
     @Override
     protected void onStart() {
